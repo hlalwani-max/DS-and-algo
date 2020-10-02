dc97e2e3a6366c929272786650922ad56e57e9e3
Harsh Lalwani <44355456+lalwani-harsh@users.noreply.github.com>
2020-10-02 03:25:12 -0700

    Adding problem description.

--- LinkedList/PalindromeList.py
+++ LinkedList/PalindromeList.py
@@ -1,8 +1,16 @@
 '''
 Linked List - https://www.interviewbit.com/problems/palindrome-list/
 Amazon, Microsoft
-
 concepts- reverse, finding mid (speed pointers)
+
+Given a singly linked list, determine if its a palindrome. Return 1 or 0 denoting if its a palindrome or not, respectively.
+
+Notes:
+Expected solution is linear in time and constant in space.
+
+For example,
+List 1-->2-->1 is a palindrome.
+List 1-->2-->3 is not a palindrome.
 '''
 
 from LinkedList import convertLToLL, printLL
