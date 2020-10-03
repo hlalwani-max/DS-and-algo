8a2be495ad99d5ed36cf379c9248937fb7a37806
Harsh Lalwani <44355456+lalwani-harsh@users.noreply.github.com>
2020-10-02 19:57:39 -0700

    Added problem description.

--- LinkedList/RemoveDuplicateFromSortedList.py
+++ LinkedList/RemoveDuplicateFromSortedList.py
@@ -1,6 +1,13 @@
 '''
 Linked List- https://www.interviewbit.com/problems/remove-duplicates-from-sorted-list/
+Microsoft, Vmware
 concept- simply iteration.
+
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
 '''
 
 from LinkedList import convertLToLL, printLL
