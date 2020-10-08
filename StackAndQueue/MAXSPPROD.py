8388cad3ace32e3dcbffc0607f0517093472809a
Harsh Lalwani <44355456+lalwani-harsh@users.noreply.github.com>
2020-10-08 13:36:40 -0700

    Stack- Maximum special product of an array (Interviewbit: NA)

--- StackAndQueue/MAXSPPROD.py
+++ StackAndQueue/MAXSPPROD.py
@@ -1,7 +1,7 @@
 '''
 https://www.interviewbit.com/problems/maxspprod/
 
-Concept-
+Concept- using stack to replace all lower values (of current value) for current value.
 
 Problem-
 You are given an array A containing N integers. The special product of each ith integer in this array is defined as the product of the following:
@@ -51,11 +51,53 @@ Explanation 2:
 class Solution:
     # @param A : list of integers
     # @return an integer
+    # TC- O(n), SC- O(n), because array is only traversed once, and elements in stack only pushed once adding total complexity to O(2n).
+    # Idea- for ith element, remove all the elements in stack that are less than or equal to ith, and add ith element to stack.
+    # Doing so, at each point you have greater value left from 0 to i-1 closest to i. Repeat same for rightValue in reverse order.
     def maxSpecialProduct(self, A):
-        pass
+        N = len(A)
+        leftVal, rightVal = [0] * N, [0] * N
+
+        # get left special array.
+        stack = []
+        for i in range(N):
+            # When processing the ith element remove all the elements from the stack which have value less than or equal to ith element.
+            while stack and A[i] >= A[stack[-1]]:
+                stack.pop()
+
+            # Since all lesser than A[i] elements are removed from stack, only greater are left. If not, default lefValue remains 0.
+            if stack and A[stack[-1]] > A[i]:
+                leftVal[i] = stack[-1]
+
+            # Add curr element to the stack.
+            stack.append(i)
+
+        # repeat above for right, but in reverse order.
+        stack.clear()
+        for i in range(N - 1, -1, -1):
+            # When processing the ith element remove all the elements from the stack which have value less than or equal to ith element.
+            while stack and A[i] >= A[stack[-1]]:
+                stack.pop()
+
+            # Since all lesser than A[i] elements are removed from stack, only greater are left. If not, default rightValue remains 0.
+            if stack and A[stack[-1]] > A[i]:
+                rightVal[i] = stack[-1]
+
+            # Add curr element to the stack.
+            stack.append(i)
+
+        del(stack)
+
+        # find max leftVal * rightVal
+        max = -1
+        for i in range(N):
+            if leftVal[i] * rightVal[i] > max:
+                max = leftVal[i] * rightVal[i]
+
+        return max % 1000000007
 
 
 if __name__ == "__main__":
-    arr = [1, 4, 3, 4]
+    arr = [6, 7, 9, 5, 5, 8]
     out = Solution().maxSpecialProduct(arr)
     print("Max special product for the input is: {}".format(out))
