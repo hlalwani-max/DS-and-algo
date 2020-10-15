ebba9d91570c3f9811b0675c1e7a441ae6897a81
Harsh Lalwani <44355456+lalwani-harsh@users.noreply.github.com>
2020-10-14 18:24:39 -0700

    Binary search code

--- Basic/binarySearch.py
+++ Basic/binarySearch.py
@@ -1,4 +1,13 @@
+'''
+Problem-
+use binary search to find target in an sorted array in logN time.
+'''
+
+
 class Solution:
+    _something = ""
+
+    # using recursion
     def binarySearch(self, arr, target, l, r):
         if l > r:
             return "Not found!!"
@@ -10,10 +19,11 @@ class Solution:
         else:
             return self.binarySearch(arr, target, l, mid - 1)
 
-
-sol = Solution()
-arr = [1, 2, 3, 4, 5]
-target = 6
-start = 0
-end = len(arr) - 1
-print(sol.binarySearch(arr, target, 0, end))
+if __name__ == "__main__":
+    sol = Solution()
+    # sorted array as input.
+    arr = [1, 2, 3, 4, 5]
+    target = 7
+    start, end = 0, len(arr) - 1
+    out = sol.binarySearch(arr, target, 0, end)
+    print(out)
